491724d3b7ff3f2e3b26970a1f140675f00129d5
Sergey Gladkovskiy <smgladkovskiy@gmail.com>
2020-04-28 18:07:34 +0300

    circleci config update

--- reader_test.go
+++ reader_test.go
@@ -141,43 +141,6 @@ func TestReadConfigs(t *testing.T) {
 		assert.EqualValues(t, refConfig, config)
 	})
 
-	if GetEnv("IN_CONTAINER", "") == "true" {
-		t.Run("Success parsing symlinked files and dirs in root", func(t *testing.T) {
-			configBytes, err := ReadConfigs("/cfgs")
-			if !assert.NoError(t, err) {
-				t.FailNow()
-			}
-
-			type cfg struct {
-				Debug bool `yaml:"debug"`
-				Log   struct {
-					Level  string `yaml:"level"`
-					Format string `yaml:"format"`
-				} `yaml:"log"`
-				Host string `yaml:"host"`
-				Port string `yaml:"port"`
-			}
-
-			config := &cfg{}
-			err = yaml.Unmarshal(configBytes, &config)
-			if !assert.NoError(t, err) {
-				t.FailNow()
-			}
-
-			refConfig := &cfg{
-				Debug: true,
-				Log: struct {
-					Level  string `yaml:"level"`
-					Format string `yaml:"format"`
-				}{Level: "warn", Format: "json"},
-				Host: "localhost",
-				Port: "8080",
-			}
-
-			assert.EqualValues(t, refConfig, config)
-		})
-	}
-
 	t.Run("Fail dir not found", func(t *testing.T) {
 		_, err := ReadConfigs("")
 		if !assert.Error(t, err) {
